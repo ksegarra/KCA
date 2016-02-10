471ab69113dd00e5b5ad084a8e37ed4c69b62d3a
Kirbybooboo <e.ryanjew@gmail.com>
2016-02-10 14:33:27 -0800

    Update week 1.py

--- Morning/week 1.py
+++ Morning/week 1.py
@@ -1,63 +1,73 @@
 #syntax
 """
 write a string
-
 type an int
-
 type a float
-
 type a boolean
-
 set a variable to 5
-
 change that variable to a 6
-
 type a list of numbers 0-9
 """
+
+"hello"
+5
+5.0
+True
+x = 5
+x = 6
+[0,1,2,3,4,5,6,7,8,9]
 
 #math
 """
 add numbers
-
 subtract numbers
-
 multiply numbers
-
 divide numbers
 """
+
+5+5
+5-5
+5*5
+5/5
 
 #variables, function call, and indexing
 """
 set variable: python to "snake"
-
 print python
-
 print: He said "I'm a student."
-
 from variable python, print the "a" character from snake
-
 print the length of variable python
-
 print two strings on one line
 """
+
+python = "snake"
+print(python)
+print("He said \"I'm a student.\"")
+print(python[2])
+print(len(python))
+print("Hello" + "there")
 
 #Comparators
 """
 check if 1 is greater than 2
-
 check if 1 is equal to 2
-
 check if 1 is greater than or equal to 2
-
 check if 1 is not equal to 2
 """
+
+1 > 2
+1 == 2
+1 >= 2
+1 != 2
 
 #Boolean Operators (and, or, not)
 """
 check if 1 is less than 2 and 2 is less than 3 on one line
-
 check if 1 is less than 2 or 2 is greater than 3 on one line
 """
+
+1 < 2 and 2 < 3
+1 < 2 or 2 > 3
 
 #If and Else statements
 """
@@ -65,9 +75,17 @@ make variable i = 5 and variable j = 10. Make an if and else statement
 that will print "foo" if i is greater than j, else print "bar"
 """
 
+i = 5
+j = 10
+if i > j:
+    print("bar")
+else:
+    print("foo")
+
 #For loops
 """
 print "a" 5 times using a for loop
 """
 
-
+for i in range(5):
+    print("a")
